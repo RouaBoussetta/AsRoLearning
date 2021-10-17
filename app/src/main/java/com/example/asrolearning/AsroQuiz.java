d24e1db37ce84ef39578157d00163e9e4483d070
Roua_Boussetta <60694832+RouaBoussetta@users.noreply.github.com>
2021-10-18 00:36:57 +0200

    adding SQLite database

---
+++ app/src/main/java/com/example/asrolearning/AsroQuiz.java
@@ -0,0 +1,20 @@
+package com.example.asrolearning;
+
+import android.provider.BaseColumns;
+
+public final class AsroQuiz {
+
+    public static class QuestionTable implements BaseColumns{
+    public  static final String TABLE_NAME="AsRoLearningQuiz";
+    public static final String COLUMN_QUESTION="question";
+    public static final String COLUMN_OPTION1="option1";
+    public static final String COLUMN_OPTION2="option2";
+    public static final String COLUMN_OPTION3="option3";
+    public static final String COLUMN_OPTION4="option4";
+    public static final String COLUMN_ANSWER="answer";
+
+
+    }
+
+
+}
